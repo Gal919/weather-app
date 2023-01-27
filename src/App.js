44d21c112e3c9b80691e6a5a6e9896337308f852
Gal919 <galraidi00@gmail.com>
2023-01-27 11:15:51 +0200

    initialize app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useSelector } from 'react-redux';
+import SearchBar from './components/SearchBar'; 
+import CurrentForecast from './components/CurrentForecast';
+import TodayForecast from './components/TodayForecast';
+import Header from './components/Header';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyles } from './components/styles/Global';
+// import { Container } from './components/styles/Global';
 
-function App() {
+const App = () => {
+
+  const currentForecast = useSelector(state => state.forecast.currentCondition);
+  const todayForecast = useSelector(state => state.forecast.todayForecast);
+  const theme = useSelector(state => state.theme);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <Header />
+        <SearchBar />
+        {currentForecast.name && <CurrentForecast />}
+        {todayForecast.list && <TodayForecast />}
+      </ThemeProvider>
     </div>
   );
 }
