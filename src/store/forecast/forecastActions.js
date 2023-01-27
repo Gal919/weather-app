8b443146f1cee16add9f4a7c6a84fa2d2ef05d21
Gal919 <galraidi00@gmail.com>
2023-01-27 17:52:49 +0200

    add loading and error cases to reducer

--- src/store/forecast/forecastActions.js
+++ src/store/forecast/forecastActions.js
@@ -1,27 +1,61 @@
-import { UPDATE_CURRENT_CONDITION, UPDATE_TODAY_FORECAST } from './forecastActionTypes';
+import { UPDATE_CURRENT_CONDITION, UPDATE_TODAY_FORECAST, UPDATE_IS_LOADING, SET_ERROR } from './forecastActionTypes';
 const apiKey = '787ef6979c61d6b7319baed8a67fff72';
 
-export const updateCurrentCondition = (city) => async (disptch) => {
-    const res = await fetch(
+export const setError = (error) => {
+  return {
+    type: SET_ERROR,
+    payload: error,
+  };
+};
+
+export const updateIsLoading = (isLoading) => {
+  return {
+    type: UPDATE_IS_LOADING,
+    payload: isLoading,
+  };
+};
+
+export const updateCurrentCondition = (city) => async (dispatch) => {
+  try {
+    const response = await fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
     );
-    const data = await res.json();
+
+    if(!response.ok) {
+      throw new Error(setError('city not found'))
+    }
    
-    disptch({
+    const data = await response.json();
+  
+    dispatch(updateIsLoading(false));
+    dispatch({
       type: UPDATE_CURRENT_CONDITION,
       payload: data,
     });
+
+  } catch (error) {
+    dispatch(setError(error.message));
   };
+};
 
-
-export const  updateTodayForecast = (city) => async (disptch) => {
-    const res = await fetch(
+export const  updateTodayForecast = (city) => async (dispatch) => {
+  try {
+    const response = await fetch(
       `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&cnt=6&appid=${apiKey}`
     );
-    const data = await res.json();
 
-    disptch({
+    if(!response.ok) {
+      throw new Error(setError('city not found'))
+    }
+    const data = await response.json();
+
+    dispatch(updateIsLoading(false));
+    dispatch({
       type: UPDATE_TODAY_FORECAST,
       payload: data,
     });
+
+  } catch (error) {
+    dispatch(setError(error.message));
   };
+};
--- src/store/forecast/forecastReducer.js
+++ src/store/forecast/forecastReducer.js
@@ -1,22 +1,34 @@
-import { UPDATE_CURRENT_CONDITION, UPDATE_TODAY_FORECAST } from './forecastActionTypes';
+import { UPDATE_CURRENT_CONDITION, UPDATE_TODAY_FORECAST, UPDATE_IS_LOADING, SET_ERROR } from './forecastActionTypes';
 
 const initialState = {
-  currentCondition: {},
-  todayForecast: {},
+  isLoading: false,
+  error: null,
+  currentCondition: null,
+  todayForecast: null,
 };
 
 export const forecastReducer = (state = initialState, action) => {
   switch (action.type) {
     case UPDATE_CURRENT_CONDITION:
-      return { 
-        ...state, 
+      return {
+        ...state,
         currentCondition: action.payload,
       };
     case UPDATE_TODAY_FORECAST:
       return {
-         ...state,
-         todayForecast: action.payload,
-        }; 
+        ...state,
+        todayForecast: action.payload,
+      };
+    case UPDATE_IS_LOADING:
+      return {
+        ...state,
+        isLoading: action.payload,
+      };
+    case SET_ERROR:
+      return {
+        state,
+        error: action.payload,
+      };
     default:
       return state;
   }
