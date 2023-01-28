5b66ea377408bf7f8d3f7a94e835505f34d99bec
Gal919 <galraidi00@gmail.com>
2023-01-28 18:45:30 +0200

    update forecast reducer

--- src/store/forecast/forecastReducer.js
+++ src/store/forecast/forecastReducer.js
@@ -12,21 +12,27 @@ export const forecastReducer = (state = initialState, action) => {
     case UPDATE_CURRENT_CONDITION:
       return {
         ...state,
+        isLoading: false,
+        error: null,
         currentCondition: action.payload,
       };
     case UPDATE_TODAY_FORECAST:
       return {
         ...state,
+        isLoading: false,
+        error: null,
         todayForecast: action.payload,
       };
     case UPDATE_IS_LOADING:
       return {
         ...state,
+        error: null,
         isLoading: action.payload,
       };
     case SET_ERROR:
       return {
         state,
+        isLoading: false,
         error: action.payload,
       };
     default:
