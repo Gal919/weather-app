dcc723e23075af540fa2301fbc2f9fb6fffe1d11
Gal919 <galraidi00@gmail.com>
2023-01-28 18:41:15 +0200

    handle empty input

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { updateCurrentCondition, updateTodayForecast, updateIsLoading } from '../store/forecast/forecastActions';
-import { Container, SearchContainer, Search } from './styles/SearchBar';
+import { updateCurrentCondition, updateTodayForecast, updateIsLoading, setError } from '../store/forecast/forecastActions';
+import { Container, SearchContainer, Search, Button } from './styles/SearchBar';
 
 const SearchBar = () => {
   const dispatch = useDispatch();
@@ -14,16 +14,30 @@ const SearchBar = () => {
 
   const handleSubmit = e => {
     e.preventDefault();
-    dispatch(updateIsLoading(true));
-    dispatch(updateCurrentCondition(city));
-    dispatch(updateTodayForecast(city));
+  
+    if(city.trim() === "") {
+      dispatch(setError('Please Enter a Value'));
+    }
+
+    else {
+      dispatch(updateIsLoading(true));
+      dispatch(updateCurrentCondition(city.trim()));
+      dispatch(updateTodayForecast(city.trim()));
+    }
   };
 
   return (
     <Container>
       <SearchContainer onSubmit={handleSubmit}>
-        <Search type='text' value={city} onChange={handleChange} />
-        <button type='submit'>Submit</button>
+        <Search
+          type='text'
+          value={city}
+          placeholder='Search for cities..'
+          onChange={handleChange}
+        />
+        <Button >
+          <i type='submit' className='fa-solid fa-magnifying-glass'></i>
+        </Button>
       </SearchContainer>
     </Container>
   );
